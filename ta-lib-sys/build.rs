b331b1fa7526cc4a4abb9d005f40a97fe1562630
Moritz Moeller <virtualritz@gmail.com>
2021-09-25 01:54:07 +0200

    Building against TA lib built from source works.

--- ta-lib-sys/build.rs
+++ ta-lib-sys/build.rs
@@ -1,11 +1,30 @@
 extern crate bindgen;
 
-use std::env;
-use std::path::PathBuf;
+use std::{env, path::PathBuf, process::Command};
 
 fn main() {
     // Tell cargo to tell rustc to link the system ta_lib
     // shared library.
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    eprintln!("{}", out_dir.to_str().unwrap());
+
+    Command::new("./configure")
+        .current_dir("ta-lib")
+        .arg(format!("--prefix={}", out_dir.to_str().unwrap()))
+        .output()
+        .expect("Failed to execute TA C library configure script");
+
+    Command::new("make")
+        .current_dir("ta-lib")
+        .arg("install")
+        .output()
+        .expect("Failed to build TA C library");
+
+    println!(
+        "cargo:rustc-link-search={}",
+        out_dir.join("lib").to_str().unwrap()
+    );
     println!("cargo:rustc-link-lib=ta_lib");
 
     // The bindgen::Builder is the main entry point
@@ -15,6 +34,7 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
+        .clang_arg(format!("-I{}", out_dir.join("include").to_str().unwrap()))
         // Generate rustified enums
         .rustified_enum(".*")
         // Finish the builder and generate the bindings.
