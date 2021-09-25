28858c6079af1c1d47763562fe744d540a018039
Moritz Moeller <virtualritz@gmail.com>
2021-09-25 17:40:26 +0200

    Updated README/docs.

--- ta-lib-sys/src/lib.rs
+++ ta-lib-sys/src/lib.rs
@@ -5,6 +5,10 @@
 //! [`ADX`](average_directional_movement_index), MACD, RSI, Stochastic,
 //! Bollinger Bands, as well as Candlestick pattern recognition.
 //!
+//! This crate includes the C source code the TA-Lib v0.4.0. This is built
+//! from source and Rust-wrapped by default. Alternatively, the system’s TA-Lib
+//! may be used. See Cargo features below.
+//!
 //! ## Cargo Features
 //! * `use_system_lib` – Use the system's installed C TA lib instead of building
 //!   from source.
